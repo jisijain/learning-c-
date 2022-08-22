c275c24c7928e81d6d41d6bd17acb1cff02900b2
jisijain <mahimajain767.mj@gmail.com>
2022-08-22 11:26:26 +0530

    add string

--- welcome.cpp
+++ welcome.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() { 
+  #include <string> 
+  string greeting = "hello";
+  cout << greeting <<endl;
   cout << "Hello Jisi in World!"<<endl;
   cout << "Hello Jisi in World!"<<endl;
   cout << "welcome back!"<<endl;
+
+
+  
   return 0;
 }
