c8ae960ee7a99af6e4897e67ab5e5fb398f1ffd7
jisijain <mahimajain767.mj@gmail.com>
2022-08-26 01:53:19 +0530

    number tree

---
+++ Loops/ForLoop/forloopQ3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n =5;
+    for (int a=0; a<n; a++){
+    for (int b=0; b<a+1;b++)
+    
+
+    cout << b ;
+    cout << " ";
+
+    cout << "\n";
+    }
+    return 0;
+
+}
