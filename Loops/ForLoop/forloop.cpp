61b650dbc2956cb1282ebb5544f4584d315cb994
jisijain <mahimajain767.mj@gmail.com>
2022-08-26 01:36:16 +0530

    forloop 0-4 easy number tree

--- Loops/ForLoop/forloop.cpp
+++ Loops/ForLoop/forloop.cpp
@@ -6,8 +6,11 @@ int main() {
     int n = 7;
 
     for (int i = 0 ; i < n ; i++) {
-        for(int j=0; j < i ; j++) 
-            cout <<  "* ";
+        for(int j=0; j <= i ; j++) 
+           
+            cout <<  i;
+             cout << " ";
+           
             
         
     cout << "\n" ;
--- Loops/ForLoop/tempCodeRunnerFile.cpp
+++ Loops/ForLoop/tempCodeRunnerFile.cpp
@@ -1,9 +1,1 @@
-int N = 3; 
-    for (int i = 0; i< N; i++ ) {
-        for (int j =0;j <i+1; j++)
-             cout<< "*";
-             cout<< " ";
-    
-           
-        //cout << "\n"; 
-    }
+ cout << " ";
