e9954838bffe16779c494b6aa837d58588e0ca34
jisijain <mahimajain767.mj@gmail.com>
2022-08-27 12:19:30 +0530

    number series question

---
+++ Loops/ForLoop/Q6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+    int n=5;
+    for (int a=0; a<=n; a++ ) {
+        for (int b=0; b<=n; b++)
+
+        {
+            if(b<a)
+            cout <<" ";
+
+            else
+            {
+                cout << b ;
+
+            }
+        }
+        cout<< "\n";
+    }
+}
