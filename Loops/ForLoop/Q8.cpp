f3592e63bafcc060fd936218fab3a03c68eedf9c
jisijain <mahimajain767.mj@gmail.com>
2022-08-30 13:53:58 +0530

    else if

---
+++ Loops/ForLoop/Q8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int n ;
+    cin>>n;
+    if ( n > 5 ) {
+            cout << "Greater than 5";
+            
+        }else if (n < 5) {
+            cout << "Less than 5";
+            
+    } else {
+        cout << "Equal to 5";
+    }
+    
+
+    return 0;
+}
